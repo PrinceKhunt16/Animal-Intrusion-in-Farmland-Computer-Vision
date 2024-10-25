201c50969dee695cc7c222b894289ddd9b2b74cd
Prince Khunt <princekhuntdatastructure@gmail.com>
2024-10-25 22:07:46 +0530

    File Structure Fix

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            "The quick brown fox jumps over the lazy dog" is an English-language pangram – a sentence that contains all the letters of the alphabet.
+        </div>
+    )
+}
+
+export default App
